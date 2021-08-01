2b359769247fb25b5a995cfe04924ff6616327db
AzizAlbaqshi <eng.albaqshi@gmail.com>
2021-08-01 18:38:44 +0300

    Task Done

--- story.js
+++ story.js
@@ -2,3 +2,4 @@ let name = "Aziz";
 let age = 32 ;
 let hobby = "football";
 console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`);
+console.log(`this is me again , Yes Im reviewing everything again .. my name is ${name},Age:${age},Hobby:${hobby} `);
